02dfc09ca581054242c4b4e7d2082365fd87f86e
marcelooagte <marcelooagte@gmail.com>
2023-11-25 09:36:22 -0300

    componente de Loading

--- Projeto_02/igniteTeams/App.tsx
+++ Projeto_02/igniteTeams/App.tsx
@@ -1,8 +1,17 @@
 import { Groups } from '@screens/Groups';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+
+import {ThemeProvider} from 'styled-components';
+import { Loading } from '@components/Loading';
+
+import theme from './src/theme';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({Roboto_400Regular, Roboto_700Bold});
   return (
-   <Groups />
+   <ThemeProvider theme={theme}>
+     {!fontsLoaded ? <Groups /> : <Loading/>}
+   </ThemeProvider>
   );
 }
 
---
+++ Projeto_02/igniteTeams/src/components/Loading/index.tsx
@@ -0,0 +1,8 @@
+import { Container, LoadingIndicator } from "./styled";
+export function Loading(){
+  return(
+    <Container>
+      <LoadingIndicator/>
+    </Container>
+  )
+}
